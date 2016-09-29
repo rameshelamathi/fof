89cd25ed70df03c21840ad1e4d9c63afcbef3597
Walt Sorensen <photodude@users.noreply.github.com>
2016-09-29 10:14:23 -0600

    remove the environments not tested against

--- Tests/environments.php
+++ Tests/environments.php
@@ -8,6 +8,4 @@
 $environments = array(
 	// The paths to Joomla cloned repo
 	'staging'     => realpath(__DIR__.'/environments/staging'),
-	'3.7.x' => realpath(__DIR__.'/environments/3.7.x'),
-	'4.0-dev' => realpath(__DIR__.'/environments/3.5-dev'),
 );
