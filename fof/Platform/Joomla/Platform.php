8aba8b795ba6c4af0882308e6d6614e55844ccb7
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2017-05-04 17:42:14 +0300

    Platform::raiseError must go through showErrorPage

--- fof/Platform/Joomla/Platform.php
+++ fof/Platform/Joomla/Platform.php
@@ -66,7 +66,7 @@ class Platform extends BasePlatform
 	 */
 	public function raiseError($code, $message)
 	{
-		throw new \Exception($message, $code);
+		$this->showErrorPage(new \Exception($message, $code));
 	}
 
 	/**
