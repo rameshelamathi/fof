3ecd0ea48929246d9d6c4d5b199bccd9fba8310f
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2017-05-06 09:26:55 +0300

    DocBlocks

--- fof/View/Compiler/Blade.php
+++ fof/View/Compiler/Blade.php
@@ -83,6 +83,14 @@ class Blade implements CompilerInterface
 		return $this->isCacheable;
 	}
 
+	/**
+	 * Compile a view template into PHP and HTML
+	 *
+	 * @param   string  $path         The absolute filesystem path of the view template
+	 * @param   array   $forceParams  Any parameters to force (only for engines returning raw HTML)
+	 *
+	 * @return mixed
+	 */
 	public function compile($path, array $forceParams = array())
 	{
 		$this->footer = array();
