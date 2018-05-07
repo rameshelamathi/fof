15ace1da4a5d56e5d1a7e95aaf6faea0701ba70f
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2018-05-07 14:23:43 +0300

    # [HIGH] DateDecorator did not work properly with add(), sub() and modify()

--- fof/Date/DateDecorator.php
+++ fof/Date/DateDecorator.php
@@ -60,14 +60,27 @@ class DateDecorator extends Date
 
 	public function __call($name, $arguments)
 	{
-		$availableMethods = array('add', 'sub', 'modify');
-
-		if (in_array($name, $availableMethods) || method_exists($this->decorated, $name))
+		if (method_exists($this->decorated, $name))
 		{
 			return call_user_func_array(array($this->decorated, $name), $arguments);
 		}
 
 		throw new \InvalidArgumentException("JDate object does not have a $name method");
+	}
+
+	public function sub(\DateInterval $interval)
+	{
+		return $this->decorated->sub($interval);
+	}
+
+	public function add(\DateInterval $interval)
+	{
+		return $this->decorated->sub($interval);
+	}
+
+	public function modify($modify)
+	{
+		return $this->decorated->modify($modify);
 	}
 
 	public function __toString()
