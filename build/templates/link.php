7415cb8b77a1dc711ac2c90d26250655686ae399
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2018-08-10 21:25:42 +0300

    Fold back translations

--- build/templates/link.php
+++ build/templates/link.php
@@ -4,6 +4,5 @@ $hardlink_files = array();
 $symlink_files = array();
 
 $symlink_folders = array(
-	'../translations/fof'            => 'translations',
 	'translations/library/fof/en-GB' => 'fof/language/en-GB',
 );
